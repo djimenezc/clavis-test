848662714b5cd0c615099621435b949de6c535b4
David Jimenez <david.calvo@clavistechnology.com>
2015-04-10 12:00:58 +0100

    - Implement Tab component

--- scripts/application.js
+++ scripts/application.js
@@ -1,24 +1,68 @@
 define([
-	'backbone',
-	'communicator',
-	'hbs!tmpl/welcome'
-],
+        'backbone',
+        'communicator',
+        'views/layout/TabLayout',
+        'hbs!tmpl/welcome'
+    ],
 
-function( Backbone, Communicator, Welcome_tmpl ) {
-    'use strict';
+    function (Backbone, Communicator, TabLayout, Welcome_tmpl) {
+        'use strict';
 
-	var welcomeTmpl = Welcome_tmpl;
+        var welcomeTmpl = Welcome_tmpl;
 
-	var App = new Backbone.Marionette.Application();
+        var App = new Backbone.Marionette.Application();
 
-	/* Add application regions here */
-	App.addRegions({});
+        /* Add application regions here */
+        App.addRegions({});
 
-	/* Add initializers here */
-	App.addInitializer( function () {
-		document.body.innerHTML = welcomeTmpl({ success: "CONGRATS!" });
-		Communicator.mediator.trigger("APP:START");
-	});
+        /* Add initializers here */
+        App.addInitializer(function () {
+            document.body.innerHTML = welcomeTmpl({success: "CONGRATS!"});
+            Communicator.mediator.trigger("APP:START");
+        });
 
-	return App;
-});
+        App.on("start", function () {
+            if (Backbone.history) {
+                Backbone.history.start();
+            }
+
+            App.views = {
+                good: new Backbone.Marionette.ItemView({
+                    class: 'good'
+                }),
+                searchable: new Backbone.Marionette.ItemView({
+                    class: 'searchable'
+                }),
+                erroneous: new Backbone.Marionette.ItemView({
+                    class: 'erroneous'
+                })
+            };
+
+            App.views.tabLayout = new TabLayout({
+                el: $("#tab"),
+                tabs: [
+                    {
+                        id: 'tab1',
+                        text: 'Good',
+                        view: App.views.good
+                    },
+                    {
+                        id: 'tab2',
+                        text: 'Searchable',
+                        view: App.views.searchable
+                    },
+                    {
+                        id: 'tab3',
+                        text: 'Erroneous',
+                        view: App.views.erroneous
+                    }
+                ]
+            });
+
+            App.views.tabLayout.render();
+
+            Communicator.mediator.trigger('displayView', 'tab1');
+        });
+
+        return App;
+    });
